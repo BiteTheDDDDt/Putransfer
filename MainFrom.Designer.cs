bbbc06d77a4e03d39c44a97eba28a282e7efbe03
BiteTheDust <952130278@qq.com>
2020-11-22 19:41:52 +0800

    add ico

---
+++ MainFrom.Designer.cs
@@ -0,0 +1,270 @@
+﻿
+namespace Putransfer
+{
+    partial class MainFrom
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainFrom));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ButtonBack = new System.Windows.Forms.Button();
+            this.ButtonSwap = new System.Windows.Forms.Button();
+            this.ButtonTransferLeft = new System.Windows.Forms.Button();
+            this.ButtonTransferRight = new System.Windows.Forms.Button();
+            this.ButtonTransferRightRight = new System.Windows.Forms.Button();
+            this.ButtonTransferLeftLeft = new System.Windows.Forms.Button();
+            this.ButtonTransfer = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(777, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(473, 710);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 12);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(473, 710);
+            this.textBox2.TabIndex = 1;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(532, 69);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(56, 172);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(695, 69);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.trackBar2.Size = new System.Drawing.Size(56, 172);
+            this.trackBar2.TabIndex = 9;
+            this.trackBar2.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("华文琥珀", 14F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.Maroon;
+            this.label2.Location = new System.Drawing.Point(38, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 25);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Putransfer";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(489, 605);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(284, 104);
+            this.panel1.TabIndex = 14;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = global::Putransfer.Properties.Resources.favicon;
+            this.pictureBox1.Location = new System.Drawing.Point(214, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(68, 67);
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // ButtonBack
+            // 
+            this.ButtonBack.Font = new System.Drawing.Font("黑体", 20F);
+            this.ButtonBack.Image = global::Putransfer.Properties.Resources.Open_6296;
+            this.ButtonBack.Location = new System.Drawing.Point(489, 367);
+            this.ButtonBack.Name = "ButtonBack";
+            this.ButtonBack.Size = new System.Drawing.Size(138, 60);
+            this.ButtonBack.TabIndex = 8;
+            this.ButtonBack.UseVisualStyleBackColor = true;
+            // 
+            // ButtonSwap
+            // 
+            this.ButtonSwap.Font = new System.Drawing.Font("黑体", 20F);
+            this.ButtonSwap.Image = global::Putransfer.Properties.Resources.LayerDiagramfile_layerdiagram_13450;
+            this.ButtonSwap.Location = new System.Drawing.Point(633, 367);
+            this.ButtonSwap.Name = "ButtonSwap";
+            this.ButtonSwap.Size = new System.Drawing.Size(138, 60);
+            this.ButtonSwap.TabIndex = 7;
+            this.ButtonSwap.UseVisualStyleBackColor = true;
+            // 
+            // ButtonTransferLeft
+            // 
+            this.ButtonTransferLeft.Font = new System.Drawing.Font("黑体", 20F);
+            this.ButtonTransferLeft.Image = global::Putransfer.Properties.Resources.MovePrevious_7195;
+            this.ButtonTransferLeft.Location = new System.Drawing.Point(532, 301);
+            this.ButtonTransferLeft.Name = "ButtonTransferLeft";
+            this.ButtonTransferLeft.Size = new System.Drawing.Size(35, 60);
+            this.ButtonTransferLeft.TabIndex = 6;
+            this.ButtonTransferLeft.UseVisualStyleBackColor = true;
+            // 
+            // ButtonTransferRight
+            // 
+            this.ButtonTransferRight.Font = new System.Drawing.Font("黑体", 20F);
+            this.ButtonTransferRight.Image = global::Putransfer.Properties.Resources.GotoNextRow_289;
+            this.ButtonTransferRight.Location = new System.Drawing.Point(695, 301);
+            this.ButtonTransferRight.Name = "ButtonTransferRight";
+            this.ButtonTransferRight.Size = new System.Drawing.Size(35, 60);
+            this.ButtonTransferRight.TabIndex = 5;
+            this.ButtonTransferRight.UseVisualStyleBackColor = true;
+            // 
+            // ButtonTransferRightRight
+            // 
+            this.ButtonTransferRightRight.Font = new System.Drawing.Font("黑体", 20F);
+            this.ButtonTransferRightRight.Image = global::Putransfer.Properties.Resources.NavigateForward_6271;
+            this.ButtonTransferRightRight.Location = new System.Drawing.Point(736, 301);
+            this.ButtonTransferRightRight.Name = "ButtonTransferRightRight";
+            this.ButtonTransferRightRight.Size = new System.Drawing.Size(35, 60);
+            this.ButtonTransferRightRight.TabIndex = 4;
+            this.ButtonTransferRightRight.UseVisualStyleBackColor = true;
+            this.ButtonTransferRightRight.Click += new System.EventHandler(this.ButtonTransferRight_Click);
+            // 
+            // ButtonTransferLeftLeft
+            // 
+            this.ButtonTransferLeftLeft.Font = new System.Drawing.Font("黑体", 20F);
+            this.ButtonTransferLeftLeft.Image = global::Putransfer.Properties.Resources.NavigateBackwards_6270;
+            this.ButtonTransferLeftLeft.Location = new System.Drawing.Point(491, 301);
+            this.ButtonTransferLeftLeft.Name = "ButtonTransferLeftLeft";
+            this.ButtonTransferLeftLeft.Size = new System.Drawing.Size(35, 60);
+            this.ButtonTransferLeftLeft.TabIndex = 3;
+            this.ButtonTransferLeftLeft.UseVisualStyleBackColor = true;
+            this.ButtonTransferLeftLeft.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // ButtonTransfer
+            // 
+            this.ButtonTransfer.Font = new System.Drawing.Font("黑体", 20F);
+            this.ButtonTransfer.Image = global::Putransfer.Properties.Resources.FillTool_204;
+            this.ButtonTransfer.Location = new System.Drawing.Point(572, 301);
+            this.ButtonTransfer.Name = "ButtonTransfer";
+            this.ButtonTransfer.Size = new System.Drawing.Size(118, 60);
+            this.ButtonTransfer.TabIndex = 2;
+            this.ButtonTransfer.UseVisualStyleBackColor = true;
+            this.ButtonTransfer.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("华文琥珀", 10.28571F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.ForeColor = System.Drawing.Color.Maroon;
+            this.label3.Location = new System.Drawing.Point(160, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 18);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "v0.1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("黑体", 7.865546F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(10, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 14);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Provided by BiteTheDDDDt";
+            // 
+            // MainFrom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1262, 734);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.ButtonBack);
+            this.Controls.Add(this.ButtonSwap);
+            this.Controls.Add(this.ButtonTransferLeft);
+            this.Controls.Add(this.ButtonTransferRight);
+            this.Controls.Add(this.ButtonTransferRightRight);
+            this.Controls.Add(this.ButtonTransferLeftLeft);
+            this.Controls.Add(this.ButtonTransfer);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainFrom";
+            this.Text = "Putransfer";
+            this.Load += new System.EventHandler(this.MainFrom_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button ButtonTransfer;
+        private System.Windows.Forms.Button ButtonTransferLeftLeft;
+        private System.Windows.Forms.Button ButtonTransferRightRight;
+        private System.Windows.Forms.Button ButtonTransferRight;
+        private System.Windows.Forms.Button ButtonTransferLeft;
+        private System.Windows.Forms.Button ButtonSwap;
+        private System.Windows.Forms.Button ButtonBack;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ MainFrom.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Putransfer
+{
+    public partial class MainFrom : Form
+    {
+        public MainFrom()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonTransferRight_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainFrom_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
